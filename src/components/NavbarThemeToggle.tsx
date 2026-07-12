cd0ac98144262bc33517c49393dc3078d2a01838
Ayushi Gautam <ayushigautam1919@gmail.com>
2026-07-12 06:28:11 +0530

    Remove rendering of ThemeToggle in NavbarThemeToggle
    Remove ThemeToggle component rendering from NavbarThemeToggle.

--- src/components/NavbarThemeToggle.tsx
+++ src/components/NavbarThemeToggle.tsx
@@ -1,13 +1,7 @@
-
 import React from 'react';
-import { ThemeToggle } from './ThemeToggle';
 
 const NavbarThemeToggle: React.FC = () => {
-  return (
-    <div className="flex items-center justify-center">
-      <ThemeToggle />
-    </div>
-  );
+  return null;
 };
 
 export default NavbarThemeToggle;
